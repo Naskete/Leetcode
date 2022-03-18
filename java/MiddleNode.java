7b89572325cdfa75ecfa3bf048ac7e6c24ddfc01
Naskete <1776795871@qq.com>
2022-03-19 00:01:54 +0800

    876. middle node

---
+++ java/MiddleNode.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ **/
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+ 
+public class MiddleNode {
+    public ListNode middleNode(ListNode head) {
+        int[] arr = new int[100];
+        ListNode p = head;
+        int index = 0;
+        while(p.next!=null){
+            arr[index++] = p.val;
+            p=p.next;
+        }
+        int idx = index%2==0?index/2:index/2+1;
+        while(idx-->0){
+            head = head.next;
+        }
+        return head;
+    }
+}
