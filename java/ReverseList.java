eeaa20ad7a66c6869e166d853828b93302b09684
Naskete <1776795871@qq.com>
2022-03-23 22:43:14 +0800

    206. reverse linked list

---
+++ java/ReverseList.java
@@ -0,0 +1,20 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode p = null, q = null;
+        while(head != null){
+            p = head;
+            head = p.next;
+            p.next = q;
+            q = p;
+        }
+        return q;
+    }
+}
