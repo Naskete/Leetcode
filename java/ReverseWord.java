b7ad5a5d14c12c1a2169bc10a09e18af6f023be4
Naskete <1776795871@qq.com>
2022-03-26 23:49:56 +0800

    151. reverse words in s string

---
+++ java/ReverseWord.java
@@ -0,0 +1,12 @@
+public class ReverseWord {
+    public static String reverseWord(String s) {
+        StringBuilder stringBuffer = new StringBuilder();
+        String[] strings = s.split(" ");
+        for (int i = strings.length -1; i>=0; i--) {
+            if(!strings[i].equals("")){
+                stringBuffer.append(strings[i]).append(" ");
+            }
+        }
+        return stringBuffer.toString().trim();
+    }
+}
