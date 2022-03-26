73b73b61171ff9adab2c9905b32f6ced224e1b48
Naskete <1776795871@qq.com>
2022-03-27 00:20:18 +0800

    70 climb stairs (动态规划)

--- java/Climb.java
+++ java/Climb.java
@@ -9,4 +9,14 @@ public class Climb {
             return climbStairs(n-1) + climbStairs(n-2);
         }        
     }
+    // 动态规划 滚动数组
+    public int climbStairsI(int n) {
+        int p = 0, q = 0, r = 1;
+        for(int i = 0; i < n; i++){
+            p = q;
+            q = r;
+            r = q + p;
+        }
+        return r;      
+    }
 }
