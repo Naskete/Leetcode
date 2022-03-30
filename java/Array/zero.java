79859ba04fa294821f3499d57d31d510b389f663
Naskete <1776795871@qq.com>
2022-03-30 23:06:19 +0800

    :refactor: simplify

--- java/Array/zero.java
+++ java/Array/zero.java
@@ -1,25 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
+package Array;
 
 public class zero{
     public void setZeroes(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        List<Integer> row = new ArrayList<>();
-        List<Integer> column = new ArrayList<>();
+        boolean[] row = new boolean[m];
+        boolean[] column = new boolean[n];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(matrix[i][j]==0){
-                    row.add(i);
-                    column.add(j);
+                    row[i] = true;
+                    column[j] = true;
                 }
             }
         }
-        for(int i = 0; i < row.size(); i++){
-            matrix[row.get(i)] = new int[n];
-        }
-        for(int k = 0; k < column.size(); k++){
-            for(int i = 0; i < m; i++){
-                matrix[i][column.get(k)] = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(row[i]||column[j]){
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
