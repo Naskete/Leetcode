9ed058f8f4e5b3717297d02ff247e95ce66cef54
Naskete <1776795871@qq.com>
2022-04-03 00:44:30 +0800

    inorder traversal

---
+++ java/BinaryTree/Inorder.java
@@ -0,0 +1,16 @@
+package BinaryTree;
+import java.util.*;
+
+public class Inorder {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        traversal(root, list);
+        return list;
+    }
+    public void traversal(TreeNode root, List<Integer> list){
+        if(root==null) return;
+        traversal(root.left, list);
+        list.add(root.val);
+        traversal(root.right, list);
+    }
+}
