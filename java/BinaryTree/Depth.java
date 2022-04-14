71031d74d4fd31be9981a4f8f666121c442200a6
Naskete <1776795871@qq.com>
2022-04-15 00:00:54 +0800

    note

--- java/BinaryTree/Depth.java
+++ java/BinaryTree/Depth.java
@@ -7,4 +7,27 @@ public class Depth {
         int right_depth = maxDepth(root.right);
         return Math.max(left_depth, right_depth) + 1;
     }
+
+    // 自上而下
+    private int answer;		// don't forget to initialize answer before call maximum_depth
+    private void maximum_depth(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            answer = Math.max(answer, depth);
+        }
+        maximum_depth(root.left, depth + 1);
+        maximum_depth(root.right, depth + 1);
+    }
+
+    // 自下而上
+    public int maximum_depth(TreeNode root) {
+        if (root == null) {
+            return 0;                                   // return 0 for null node
+        }
+        int left_depth = maximum_depth(root.left);
+        int right_depth = maximum_depth(root.right);
+        return Math.max(left_depth, right_depth) + 1;	// return depth of the subtree rooted at root
+    }
 }
