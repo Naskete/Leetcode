772a01b8e841e970fee74574625eb2bc4812d78a
Naskete <1776795871@qq.com>
2022-04-28 15:49:40 +0800

    112. path sum

---
+++ java/BinaryTree/PathValue.java
@@ -0,0 +1,22 @@
+package BinaryTree;
+
+public class PathValue {
+    
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false;
+        }
+        return pathValue(root, targetSum, 0);
+    }
+
+    public boolean pathValue(TreeNode root, int target, int sum){
+        if(root == null){
+            return false;
+        }
+        sum += root.val;
+        if(root.left==null && root.right == null){
+            return sum==target;
+        }
+        return pathValue(root.left, target, sum) || pathValue(root.right, target, sum);
+    }
+}
