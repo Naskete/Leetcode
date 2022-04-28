af0bada7ac80f1745f795e9563ac4bfa4e1ea3ab
Naskete <1776795871@qq.com>
2022-04-28 10:40:15 +0800

    add iterator: inorder

--- java/BinaryTree/Inorder.java
+++ java/BinaryTree/Inorder.java
@@ -13,4 +13,21 @@ public class Inorder {
         list.add(root.val);
         traversal(root.right, list);
     }
+
+    public List<Integer> inorder(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while(!stack.empty() || node != null){
+            while(node != null){
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            list.add(node.val);
+            node = node.right;
+        }
+        return list;
+    }
 }
