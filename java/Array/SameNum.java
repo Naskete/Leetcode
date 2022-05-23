bc7acf4a466d72a9459a02f8a47e6ca7b4d1bacf
Naskete <1776795871@qq.com>
2022-05-24 00:15:17 +0800

    the same num in the array

---
+++ java/Array/SameNum.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class SameNum{
+    public int findRepeatNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] == i) {
+                i++;
+                continue;
+            }
+            if (nums[nums[i]] == nums[i])
+                return nums[i];
+            int tmp = nums[i];
+            nums[i] = nums[tmp];
+            nums[tmp] = tmp;
+        }
+        return -1;
+    }
+}
