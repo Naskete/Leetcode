98a6122c88b705c9f8239ce8f08eca9ff0d08740
Naskete <1776795871@qq.com>
2022-07-10 22:38:50 +0800

    4ms -> 0ms

--- java/BinaryTree/ValidBST.java
+++ java/BinaryTree/ValidBST.java
@@ -2,38 +2,16 @@ package BinaryTree;
 
 public class ValidBST {
     public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long low, long up) {
         if (root == null) {
             return true;
         }
-        boolean left = true, right = true;
-        if (root.left != null) {
-            left = root.val > getMax(root.left);
+        if (root.val <= low || root.val >= up) {
+            return false;
         }
-        if (root.right != null) {
-            right = root.val < getMin(root.right);
-        }
-        return left && right && isValidBST(root.left) && isValidBST(root.right);
-    }
-    
-    public int getMax(TreeNode root) {
-        int leftmax = Integer.MIN_VALUE, rightmax = Integer.MIN_VALUE;;
-        if (root == null) {
-            return Integer.MIN_VALUE;
-        } else {
-            leftmax = Math.max(root.val, getMax(root.left));
-            rightmax = Math.max(root.val, getMax(root.right));
-        }
-        return Math.max(leftmax, rightmax);
-    }
-    
-    public int getMin(TreeNode root) {
-        int leftmin = Integer.MAX_VALUE, rightmin = Integer.MAX_VALUE;;
-        if (root == null) {
-            return Integer.MAX_VALUE;
-        } else {
-            leftmin = Math.min(root.val, getMin(root.left));
-            rightmin = Math.min(root.val, getMin(root.right));
-        }
-        return Math.min(leftmin, rightmin);
+        return isValidBST(root.left, low, root.val) && isValidBST(root.right, root.val, up);
     }
 }
