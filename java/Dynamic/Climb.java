3e07f0a37a59486e0f2c91f158aa405d54c3e6c4
Naskete <1776795871@qq.com>
2022-07-14 23:57:58 +0800

    7.14 update

--- java/Dynamic/Climb.java
+++ java/Dynamic/Climb.java
@@ -12,12 +12,17 @@ public class Climb {
     }
     // 动态规划 滚动数组
     public int climbStairsI(int n) {
-        int p = 0, q = 0, r = 1;
-        for(int i = 0; i < n; i++){
-            p = q;
-            q = r;
-            r = q + p;
+        int p = 1, q = 1, r = 1;
+        for (int i = 1; i < n; i++) {
+            r = p + q;
+            q = p;
+            p = r;
         }
-        return r;      
+        return r;
+        // if (n <= 2) {
+        //     return n;
+        // } else {
+        //     return climbStairs(n - 1) + climbStairs(n - 2);
+        // }     
     }
 }
