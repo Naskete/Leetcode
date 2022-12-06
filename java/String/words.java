915444e643cbb0e607159941ab85eccd7d5baaa2
Naskete <1776795871@qq.com>
2022-12-06 22:44:44 +0800

    1805 add double pointer soultion

--- java/String/words.java
+++ java/String/words.java
@@ -19,6 +19,25 @@ public class words {
 
     // double pointer
     public int numDifferentIntegersDoublePointer(String word) {
-        return 0;
+        Set<String> set = new HashSet<String>();
+        int n = word.length(), p1 = 0, p2;
+        while (true) {
+            while (p1 < n && !Character.isDigit(word.charAt(p1))) {
+                p1++;
+            }
+            if (p1 == n) {
+                break;
+            }
+            p2 = p1;
+            while (p2 < n && Character.isDigit(word.charAt(p2))) {
+                p2++;
+            }
+            while (p2 - p1 > 1 && word.charAt(p1) == '0') { // 去除前导 0
+                p1++;
+            }
+            set.add(word.substring(p1, p2));
+            p1 = p2;
+        }
+        return set.size();
     }
 }
