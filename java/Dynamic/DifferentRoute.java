f93a5a160edfdfd936ae54149993bbaad8bcc8eb
Naskete <1776795871@qq.com>
2022-12-06 23:19:56 +0800

    62 unique paths in math

--- java/Dynamic/DifferentRoute.java
+++ java/Dynamic/DifferentRoute.java
@@ -17,5 +17,18 @@ public class DifferentRoute {
             }
         }
         return dp[m - 1][n - 1];
-    } 
+    }
+
+    // 数学组合
+    // 从左上角到右下角的过程中，我们需要移动 m+n-2m+n−2 次，其中有 m-1m−1 次向下移动，n-1n−1 次向右移动。因此路径的总数，
+    // 就等于从 m+n-2m+n−2 次移动中选择 m-1m−1 次向下移动的方案数，即组合数
+    // (m+n-2)(m+n-3)···n / (m-1)!
+
+    public int uniquePath(int m, int n) {
+        long ans = 1;
+        for (int x = n, y = 1; y < m; ++x, ++y) {
+            ans = ans * x / y;
+        }
+        return (int)ans;
+    }
 }
