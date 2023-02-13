5ce1c298b60fe3e1b2bcae2ea19cb46dc3a2e72f
naskete <1776795871@qq.com>
2023-02-13 23:00:34 +0800

    get middle or right-mid node in list

--- review/MiddleNode.java
+++ review/MiddleNode.java
@@ -4,7 +4,7 @@ public class MiddleNode {
     // 奇数返回中点，偶数返回上中点，中间左边的点
     public ListNode getMiddleOrLeftMideNode(ListNode head) {
         if (head == null || head.next == null || head.next.next == null) return head;
-        // 有三个节点以上
+        // 有三个或以上节点
         ListNode slow = head.next;
         ListNode fast = head.next.next;
         while (fast.next != null && fast.next.next != null) {
@@ -14,7 +14,15 @@ public class MiddleNode {
         return slow;
     }
 
-    public ListNode getMiddleRightNode(ListNode head) {
-        return null;
+    // 奇数返回中点，偶数返回下中点，中间右边的点
+    public ListNode getMiddleOrRightMiddleNode(ListNode head) {
+        if (head == null ||head.next == null) return head;
+        ListNode slow = head.next;
+        ListNode fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
