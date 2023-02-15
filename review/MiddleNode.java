886646becd5ecd8357e3f76b390f390e81b5a0fa
naskete <1776795871@qq.com>
2023-02-15 23:01:07 +0800

    get pre of middle or right-mid node in list

--- review/MiddleNode.java
+++ review/MiddleNode.java
@@ -25,4 +25,35 @@ public class MiddleNode {
         }
         return slow;
     }
+
+    // 奇数返回中点前一个，偶数返回上中点前一个，中间左边的前一个点
+    public ListNode getMiddleOrLeftMidPreNode(ListNode head) {
+        if (head == null || head.next == null && head.next.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // 奇数返回中点前一个，偶数返回下中点前一个，中间右边的前一个点
+    public ListNode getMiddleOrRightMidNextNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        if (head.next.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return null;
+    }
 }
