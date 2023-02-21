43125abc37ece59d44fd7518dd93c749a8579de8
naskete <1776795871@qq.com>
2023-02-21 22:14:52 +0800

    O(logn) solution

--- java/Array/SingleInSortedArray.java
+++ java/Array/SingleInSortedArray.java
@@ -9,4 +9,36 @@ public class SingleInSortedArray {
         }
         return single;
     }
+
+    // 异或求解
+    // 但是这里给的是有序的列表
+    public int singleNonDuplicateInSortedArray(int[] nums) {
+        
+        int left=0,right=nums.length-1;
+        while(left<right){
+          int mid=left+(right-left)/2;
+          boolean iseven=(right-mid)%2==0;
+          if(nums[mid]==nums[mid-1]){
+             if(iseven){
+                 right=mid-2;//ignore the duplicate element
+             }
+             else{
+                 left=mid+1;
+             }
+          }
+          else if(nums[mid]==nums[mid+1])
+          {
+              if(iseven){
+                  left=mid+2;
+              }else{
+                  right=mid-1;
+              }
+          }else{
+              return nums[mid];
+          }
+            
+
+        }
+        return nums[left];
+    }
 }
