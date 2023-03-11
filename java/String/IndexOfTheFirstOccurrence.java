3053c42d8b263978c9fbbaf43298f5f3b79b04d2
naskete <1776795871@qq.com>
2023-03-11 23:46:36 +0800

    2023-03-11 28 find the index of the first occurrence in a string

---
+++ java/String/IndexOfTheFirstOccurrence.java
@@ -0,0 +1,21 @@
+package String;
+
+public class IndexOfTheFirstOccurrence {
+    // 28 https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
