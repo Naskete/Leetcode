8788ad37754329acadb5eeefd6ceae30cf066949
naskete <1776795871@qq.com>
2023-03-14 23:29:47 +0800

    sum root to leaf numbers

--- java/BinaryTree/PathSum.java
+++ java/BinaryTree/PathSum.java
@@ -31,4 +31,21 @@ public class PathSum {
             getNumber(root.right, val, list);
         }
     }
+
+    // offical Soulution
+    public int sumNumbersI(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int prevSum) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+    }
 }
