343dcc1f9b67a837a6e9c448e0b453f8e7ddfcf5
naskete <1776795871@qq.com>
2023-03-16 23:56:07 +0800

    101 symmetric tree

--- java/BinaryTree/Symmetric.java
+++ java/BinaryTree/Symmetric.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 
+import java.util.*;
+
 public class Symmetric {
     public boolean isSymmetric(TreeNode root) {
         return isSymmetric(root, root);
@@ -16,5 +18,32 @@ public class Symmetric {
                 return false;
             }
         }
+    }
+    public boolean check(TreeNode p, TreeNode q){
+        // if(p==null && q==null){
+        //     return true;
+        // }
+        // if(p==null || q==null){
+        //     return false;
+        // }
+        // return p.val==q.val&&check(p.left, q.right)&&check(p.right, q.left);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(p);
+        queue.offer(q);
+        while(!queue.isEmpty()){
+            p = queue.poll();
+            q = queue.poll();
+            if(p == null && q == null){
+                continue;
+            }
+            if(p == null || q == null || q.val != p.val){
+                return false;
+            }
+            queue.add(p.left);
+            queue.add(q.right);
+            queue.add(p.right);
+            queue.add(q.left);
+        }
+        return true;
     }
 }
