76851abb21abd3f8413851bc6327a4a9bdf2526c
naskete <1776795871@qq.com>
2023-03-26 00:04:59 +0800

    912 sorted array: quick sort

--- java/Array/Sorted.java
+++ java/Array/Sorted.java
@@ -41,4 +41,55 @@ public class Sorted {
             nums[left + i] = help[i];
         }
     }
+
+    // quick sort
+
+    public void quickSort(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        process(nums, 0, nums.length - 1);
+    }
+
+    public void quickProcess(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        swap(nums, right, (int)(Math.random() * (right - left + 1)) + left);
+        int[] equals = getEquals(nums, left, right);
+        quickProcess(nums, left, equals[0] - 1);
+        quickProcess(nums, equals[1] + 1, right);
+    }
+
+    public int[] getEquals(int[] nums, int L, int R) {
+        if (L > R) {
+            return new int[] {-1, -1};
+        }
+
+        if (L == R) {
+            return new int[] {L, L};
+        }
+        int flag = nums[R];
+        int less = L - 1, more = R;
+        int idx = L;
+        while (idx < more) {
+            if (nums[idx] > flag) {
+                swap(nums, idx, --more);
+            } else if (nums[idx] < flag) {
+                swap(nums, ++less, idx++);
+            } else {
+                idx++;
+            }
+        }
+        swap(nums, R, more);
+        return new int[]{less + 1, more};
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+
 }
