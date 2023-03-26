f502658d5f4a25e60770edb30cfbde301c79aa09
naskete <1776795871@qq.com>
2023-03-27 00:26:10 +0800

    16.26 add calculator-lcci

--- java/String/BasicCalculator.java
+++ java/String/BasicCalculator.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 // 224. basic calculate
 public class BasicCalculator {
-    public int calculate(String s) {
+    public int basicCalculate(String s) {
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
         int sign = 1;
@@ -35,6 +35,51 @@ public class BasicCalculator {
             } else {
                 idx++;
             }
+        }
+        return res;
+    }
+
+    // 面试 16.26 
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int sign = 1;
+        int idx = 0;
+        char[] array = s.toCharArray();
+        int n = array.length;
+        int res = 0;
+        int num = 0;
+        while (idx < n) {
+            if (Character.isDigit(array[idx])) {
+                while (idx < n && Character.isDigit(array[idx])) {
+                    num = num * 10 + (array[idx] - '0');
+                    idx++;
+                }
+                idx--;
+                if (sign <= 1) {
+                    stack.push(sign * num);
+                } else {
+                    if (sign == 2) {
+                        stack.push(stack.pop() * num);
+                    } else {
+                        stack.push(stack.pop() / num);
+                    }
+                }
+            }  else {
+                if (array[idx] == '+') {
+                    sign = 1;
+                } else if (array[idx] == '-') {
+                    sign = -1;
+                } else if (array[idx] == '*') {
+                    sign = 2;
+                } else if (array[idx] == '/') {
+                    sign = 3;
+                }
+                num = 0;
+            }
+            idx++;
+        }
+        while (!stack.isEmpty()) {
+            res += stack.pop();
         }
         return res;
     }
