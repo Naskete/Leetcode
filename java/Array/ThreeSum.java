cfb9a5ffef7f23651f602f988556cbbe596cfae6
naskete <1776795871@qq.com>
2023-07-09 22:48:27 +0800

    15. three sum

--- java/Array/ThreeSum.java
+++ java/Array/ThreeSum.java
@@ -37,4 +37,44 @@ public class ThreeSum {
         }
         return list;
     }
+
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            Arrays.sort(nums);
+            List<List<Integer>> list = new ArrayList<>();
+            int idx = 0, n = nums.length;
+            while (idx < n) {
+                if (nums[idx] > 0) {
+                    return list;
+                }
+                while (idx > 0 && idx < n && nums[idx] == nums[idx - 1]) {
+                    idx++;
+                }
+                int left = idx + 1, right = n - 1;
+                while(left < right) {
+                    if (nums[idx] + nums[left] + nums[right] == 0) {
+                        List<Integer> ans = new ArrayList<>();
+                        ans.add(nums[idx]);
+                        ans.add(nums[left]);
+                        ans.add(nums[right]);
+                        list.add(ans);
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (nums[idx] + nums[left] + nums[right] > 0) {
+                        right--;
+                    } else if (nums[idx] + nums[left] + nums[right] < 0) {
+                        left++;
+                    }
+                }
+                idx++;
+            }
+            return list;
+        }
+    }
 }
